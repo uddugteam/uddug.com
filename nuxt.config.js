bb7d6a1f797186a5407f36bcf0e56d7eee1ca1d0
Andrew Skurlatov <a.skurlatov@gmail.com>
2020-08-13 01:48:56 +0300

    done: remove bootstrap-vue

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,8 +55,6 @@ export default {
   ** Nuxt.js modules
   */
   modules: [
-    // Doc: https://bootstrap-vue.js.org
-    'bootstrap-vue/nuxt',
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
     '@nuxtjs/pwa'
