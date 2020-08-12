ffd470877a6f9d12cf0e367f0a737ed1d9f2a073
Andrew Skurlatov <a.skurlatov@gmail.com>
2020-08-12 12:50:58 +0300

    wip: services block

--- pages/index.vue
+++ pages/index.vue
@@ -1,44 +1,74 @@
 <template>
-  <header class="container musthead">
-    <h1 class="musthead__title">
-      <span class="musthead__underliner"> Production</span>-focused Development <span class="musthead__underliner">Team</span>
-    </h1>
-    <div class="row musthead__about">
-      <div class="col-2 col-sm-1 order-sm-1">
-        <h3 class="musthead__about__title">
-          About
-        </h3>
-      </div>
-      <div class="col-10 col-sm-5 order-sm-2 col-md-6 col-lg-5">
-        <p class="musthead__about__text">We combine proven methodologies, business domain knowledge and technology expertise of skilled software professionals to deliver highly optimized solutions and services across wide range of industry domains.</p>
-      </div>
-      <div class="offset-sm-1 order-sm-3 d-none d-sm-block d-md-none" />
-      <div class="offset-2 d-block d-sm-none" />
-      <div class="col-10 col-sm-4 order-sm-6 order-md-3 col-lg-3 ">
-        <p class="musthead__about__text">Our projects are different, but all of them share the need to have a software solution</p>
-      </div>
-      <div class="offset-lg-3 order-lg-4" />
-      <div class="offset-lg-1 order-lg-4" />
-      <div class="col-12 col-sm-5 order-sm-4 order-md-4 col-md-4 col-lg-3">
-        <div class="musthead__about__item">
-          <p><span class="break-line">Experienced</span><span class="break-line"> and cross-functional</span></p>
+  <div>
+    <header class="container musthead">
+      <h1 class="musthead__title">
+        <span class="musthead__underliner"> Production</span>-focused Development <span class="musthead__underliner musthead__title__team">Team</span>
+      </h1>
+      <div class="row musthead__about">
+        <div class="col-2 col-sm-1 order-sm-1">
+          <h3 class="musthead__about__title">
+            About
+          </h3>
+        </div>
+        <div class="col-10 col-sm-5 order-sm-2 col-md-6 col-lg-5">
+          <p class="musthead__about__text">We combine proven methodologies, business domain knowledge and technology expertise of skilled software professionals to deliver highly optimized solutions and services across wide range of industry domains.</p>
+        </div>
+        <div class="offset-sm-1 order-sm-3 d-none d-sm-block d-md-none" />
+        <div class="offset-2 d-block d-sm-none" />
+        <div class="col-10 col-sm-4 order-sm-6 order-md-3 col-lg-3 ">
+          <p class="musthead__about__text">Our projects are different, but all of them share the need to have a software solution</p>
+        </div>
+        <div class="offset-lg-3 order-lg-4 d-md-none d-lg-block" />
+        <div class="offset-lg-1 order-lg-4 d-md-none d-lg-block" />
+        <div class="col-12 col-sm-5 order-sm-4 order-md-4 col-md-4 col-lg-3">
+          <div class="musthead__about__item">
+            <p class="musthead__about__item__text"><span class="break-line">Experienced</span><span class="break-line"> and cross-functional</span></p>
+          </div>
+        </div>
+        <div class="offset-sm-1 order-sm-5 d-none d-sm-block d-md-none" />
+        <div class="offset-sm-2 order-sm-7 d-none d-sm-block d-md-none" />
+        <div class="col-12 col-sm-5 order-sm-8 col-md-4">
+          <div class="musthead__about__item musthead__about__item-centre">
+            <p class="musthead__about__item__text"><span class="break-line">Modern stack</span><span class="break-line"> (latest technology trends)</span></p>
+          </div>
+        </div>
+        <div class="offset-sm-7 order-sm-9 d-none d-sm-block d-md-none" />
+        <div class="col-12 col-sm-5 order-sm-10 mr-sm-auto col-md-4 col-lg-3">
+          <div class="musthead__about__item musthead__about__item-last">
+            <p class="musthead__about__item__text"><span class="break-line">Trusted</span><span class="break-line"> development partner</span></p>
+          </div>
         </div>
       </div>
-      <div class="offset-sm-1 order-sm-5 d-none d-sm-block d-md-none" />
-      <div class="offset-sm-2 order-sm-7 d-none d-sm-block d-md-none" />
-      <div class="col-12 col-sm-5 order-sm-8 col-md-4">
-        <div class="musthead__about__item musthead__about__item-centre">
-          <p><span class="break-line">Modern stack</span><span class="break-line"> (latest technology trends)</span></p>
+    </header>
+    <section class="container services">
+      <div class="row">
+        <div class="col-12 col-sm-6 col-lg-3">
+          <div class="services__item services__item-consulting">
+            <h3 class="services__item__title">Technical Consulting</h3>
+            <p class="services__item__text">Advice on the strategic employment of modern technologies based on a thorough analysis and experience.</p>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-3">
+          <div class="services__item services__item-cloud">
+            <h3 class="services__item__title">Cloud Infrastructure</h3>
+            <p class="services__item__text">Using DevOps best practises such to design and implement most related solutions for all major cloud providers.</p>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-3">
+          <div class="services__item services__item-ml">
+            <h3 class="services__item__title">Machine Learning</h3>
+            <p class="services__item__text">Modern applications for all platforms builded with world best design patterns and technologies.</p>
+          </div>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-3">
+          <div class="services__item services__item-blockchain">
+            <h3 class="services__item__title">Blockchain Developing</h3>
+            <p class="services__item__text">Decentralized applications and distributed services powered by public and private blockchain networks.</p>
+          </div>
         </div>
       </div>
-      <div class="offset-sm-7 order-sm-9 d-none d-sm-block d-md-none" />
-      <div class="col-12 col-sm-5 order-sm-10 mr-sm-auto col-md-4 col-lg-3">
-        <div class="musthead__about__item musthead__about__item-last">
-          <p><span class="break-line">Trusted</span><span class="break-line"> development partner</span></p>
-        </div>
-      </div>
-    </div>
-  </header>
+    </section>
+  </div>
 </template>
 
 <script lang="ts">
@@ -55,10 +85,18 @@ export default Vue.extend({})
   padding-top: 143px;
   padding-bottom: 122px;
 
+  @include media-breakpoint-only(md) {
+    padding-bottom: 130px;
+  }
+
+  @include media-breakpoint-down(sm) {
+    padding-bottom: 80px;
+  }
+
   &__underliner {
     // TODO need corner radius for underline
     text-decoration: underline;
-    text-decoration-color: $lighter;
+    text-decoration-color: $scarleter;
   }
 
   &__title {
@@ -81,6 +119,12 @@ export default Vue.extend({})
 
     @include media-breakpoint-only(sm) {
       font-size: 48px;
+    }
+
+    &__team {
+      @include media-breakpoint-down(xs) {
+        display: block;
+      }
     }
   }
 
@@ -128,7 +172,7 @@ export default Vue.extend({})
     &__item {
       background: $dark;
       transition: background-color 0.5s, color 0.5s, box-shadow 0.5s;
-      padding: 25px 25px 25px;
+      padding: 25px 0 25px 32px;
       border-radius: 10px;
       font-family: 'Montserrat', sans-serif;
       font-style: normal;
@@ -136,13 +180,12 @@ export default Vue.extend({})
       font-size: 15px;
       line-height: 120%;
       color: $white;
-
       cursor: help;
 
       &:hover {
         background: $white;
         color: $darker;
-        box-shadow: 0px 10px 20px rgba(0, 0, 0, 0.1);
+        box-shadow: 0 10px 20px rgba(0, 0, 0, 0.1);
       }
 
       @include media-breakpoint-up(lg) {
@@ -153,26 +196,29 @@ export default Vue.extend({})
         margin-top: 99px;
       }
 
-      @include media-breakpoint-down(sm) {
-        font-size: 14px;
-        margin-bottom: 23px;
+      @include media-breakpoint-only(md) {
+        padding-left: 57px;
       }
 
-      @include media-breakpoint-down(xs) {
+      @include media-breakpoint-only(sm) {
+        font-size: 14px;
+        margin-bottom: 23px;
+        padding-left: 45px;
+      }
+
+      @include media-breakpoint-only(xs) {
         font-size: 16px;
         margin-bottom: 10px;
       }
 
-      // TODO need to change way to show bullets
-      /*p::before {
-        content: "• ";
-        color: $lighter;
-        font-size: 25px;
-        font-weight: bolder;
-        display: inline-block;
-        width: 13px;
-        margin-left: -13px;
-      }*/
+      &__text {
+        padding-left: 24px;
+        background: transparent url("/bullet.svg") no-repeat 0 3px;
+
+        @include media-breakpoint-up(sm) {
+          padding-left: 21px;
+        }
+      }
     }
 
     &__item-centre {
@@ -180,8 +226,13 @@ export default Vue.extend({})
         padding-left: 55px;
       }
 
+      @include media-breakpoint-only(md) {
+        padding-left: 37.5px;
+      }
+
       @include media-breakpoint-only(sm) {
         margin-top: -67px;
+        padding-left: 28.5px;
       }
     }
 
@@ -190,6 +241,54 @@ export default Vue.extend({})
         margin-top: -67px;
       }
     }
+  }
+}
+
+.services {
+  padding-bottom: 50px;
+  border-bottom: 1px solid $lighter;
+
+  &__item {
+    margin-bottom: 50px;
+    padding-left: 56px;
+
+    @include media-breakpoint-up(lg) {
+      padding-left: 0;
+      padding-top: 56px;
+    }
+
+    &__title {
+      font-family: 'Montserrat', sans-serif;
+      font-size: 16px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: 130%;
+      letter-spacing: -0.02em;
+      color: $darker;
+      margin-bottom: 5px;
+    }
+
+    &__text {
+      font-size: 14px;
+      line-height: 140%;
+      color: $browner;
+    }
+  }
+
+  &__item-consulting {
+    background: transparent url("/consulting.svg") no-repeat 0 2px;
+  }
+
+  &__item-cloud {
+    background: transparent url("/cloud.svg") no-repeat 0 2px;
+  }
+
+  &__item-ml {
+    background: transparent url("/ml.svg") no-repeat 0 2px;
+  }
+
+  &__item-blockchain {
+    background: transparent url("/blockchain.svg") no-repeat 0 2px;
   }
 }
 </style>
