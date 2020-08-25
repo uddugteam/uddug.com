a7bea409845c1dbc18ee7647c6c47c470eaa48eb
Andrew Skurlatov <a.skurlatov@gmail.com>
2020-08-26 00:30:43 +0300

    done: remove base router path

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,9 +10,6 @@ export default {
   ** See https://nuxtjs.org/api/configuration-target
   */
   target: 'static',
-  router: {
-    base: '/uddug.com/'
-  },
   /*
   ** Headers of the page
   ** See https://nuxtjs.org/api/configuration-head
